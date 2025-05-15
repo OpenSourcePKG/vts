05ef888a1326708461de557443b4b2a158aaf615
spe <stefan.pegenau@pegenau.de>
2025-05-15 15:12:24 +0200

    Recompiled everything.

---
+++ dist/schemas/enumSchema.d.ts
@@ -0,0 +1,11 @@
+import { Schema, SchemaDescription, SchemaErrors, SchemaOptions } from '../schema.js';
+export interface EnumSchemaDescription extends SchemaDescription {
+    type: 'enum';
+    values: Record<string, SchemaDescription>;
+}
+export declare class EnumSchema<T extends string> extends Schema<T> {
+    private readonly _enum;
+    constructor(_enum: Record<string, T>, _options?: SchemaOptions);
+    describe(): EnumSchemaDescription;
+    validate(_data: unknown, _errors: SchemaErrors, _options?: SchemaOptions): _data is T;
+}
---
+++ dist/schemas/enumSchema.js
@@ -0,0 +1,38 @@
+import { Schema } from '../schema.js';
+import { Vts } from '../vts.js';
+export class EnumSchema extends Schema {
+    _enum;
+    constructor(_enum, _options) {
+        super(_options);
+        this._enum = _enum;
+    }
+    describe() {
+        const schemaDescription = super.describe();
+        const enumSchemaDescription = {
+            ...schemaDescription,
+            type: 'enum',
+            values: {}
+        };
+        for (const [k, t] of Object.entries(this._enum)) {
+            enumSchemaDescription.values[t] = {
+                description: k
+            };
+        }
+        return enumSchemaDescription;
+    }
+    validate(_data, _errors, _options) {
+        const enumErrors = {};
+        for (const [key, type] of Object.entries(this._enum)) {
+            const errors = [];
+            if (Vts.equal(type, _options).validate(_data, errors)) {
+                return true;
+            }
+            if (errors.length > 0) {
+                enumErrors[`schema${key}`] = errors;
+            }
+        }
+        this.addError(_errors, 'no match with any of the given schemas');
+        this.addError(_errors, enumErrors);
+        return false;
+    }
+}
